54761b31056f91587535ec312c37958f00ee72c4
lesnikovskiy <lesnikovski@gmail.com>
2017-05-12 22:03:52 +0300

    Sending Requests with XMLHttpRequest

--- main.ts
+++ main.ts
@@ -1,53 +1,28 @@
 import { Observable } from "rxjs";
 
-let circle = document.getElementById("circle");
-let source = Observable.fromEvent(document, "mousemove")
-    .map((e: MouseEvent) => {
-        return {
-            x: e.clientX,
-            y: e.clientY
-        };
-    })
-    .filter(value => value.x < 500)
-    .delay(200);
+const output = document.getElementById("output");
+const button = document.getElementById("button");
 
-function onNext(value) {
-    circle.style.left = `${value.x}px`;
-    circle.style.top = `${value.y}px`;
+let click = Observable.fromEvent(button, "click");
+
+function load(url: string) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach(m => {
+            let div = document.createElement("div");
+            div.innerHTML = m.title;
+            output.appendChild(div);
+        });
+    });
+
+    xhr.open("GET", url);
+    xhr.send();
 }
 
-source.subscribe(
-    onNext,
+click.subscribe(
+    e => load("movies.json"),
     error => console.log(`error: ${error}`),
     () => console.log("complete")
 );
-
-// import { Observable } from "rxjs";
-
-// // drawing example
-// let circle = document.getElementById("circle");
-// let source = Observable.fromEvent(document, "mousemove")
-//     .map((e: MouseEvent) => {
-//         return {
-//             which: e.which,
-//             x: e.clientX,
-//             y: e.clientY
-//         };
-//     })
-//     .filter(value => value.which === 1)
-//     .delay(80);
-
-// function onNext(value) {
-//     circle.style.left = `${value.x}px`;
-//     circle.style.top = `${value.y}px`;
-
-//     let element = circle.cloneNode(true);
-//     document.body.appendChild(element);
-// }
-
-// source.subscribe(
-//     onNext,
-//     error => console.log(`error: ${error}`),
-//     () => console.log("complete")
-// );
-
