a3fab512634c3abd899bde2da4cd5a7c68f01031
lesnikovskiy <lesnikovski@gmail.com>
2017-05-12 21:37:27 +0300

    Processing Mouse Events + Drawing excerpt of code

--- main.ts
+++ main.ts
@@ -1,23 +1,53 @@
 import { Observable } from "rxjs";
 
-const numbers = [1, 5, 10];
-let source = Observable.create(observer => {
-   let index = 0;
-   let produceValue = () => {
-        observer.next(numbers[index++]);
+let circle = document.getElementById("circle");
+let source = Observable.fromEvent(document, "mousemove")
+    .map((e: MouseEvent) => {
+        return {
+            x: e.clientX,
+            y: e.clientY
+        };
+    })
+    .filter(value => value.x < 500)
+    .delay(200);
 
-        if (index < numbers.length) {
-            setTimeout(produceValue, 250);
-        } else {
-            observer.complete();
-        }
-   };
-
-   produceValue();
-}).map(n => n * 2).filter(n => n > 4);
+function onNext(value) {
+    circle.style.left = `${value.x}px`;
+    circle.style.top = `${value.y}px`;
+}
 
 source.subscribe(
-    value => console.log(`value: ${value}`),
+    onNext,
     error => console.log(`error: ${error}`),
     () => console.log("complete")
 );
+
+// import { Observable } from "rxjs";
+
+// // drawing example
+// let circle = document.getElementById("circle");
+// let source = Observable.fromEvent(document, "mousemove")
+//     .map((e: MouseEvent) => {
+//         return {
+//             which: e.which,
+//             x: e.clientX,
+//             y: e.clientY
+//         };
+//     })
+//     .filter(value => value.which === 1)
+//     .delay(80);
+
+// function onNext(value) {
+//     circle.style.left = `${value.x}px`;
+//     circle.style.top = `${value.y}px`;
+
+//     let element = circle.cloneNode(true);
+//     document.body.appendChild(element);
+// }
+
+// source.subscribe(
+//     onNext,
+//     error => console.log(`error: ${error}`),
+//     () => console.log("complete")
+// );
+
