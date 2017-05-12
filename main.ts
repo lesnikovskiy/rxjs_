74825035e053eeb65ce264578cdb8fb89ff309a2
lesnikovskiy <lesnikovski@gmail.com>
2017-05-12 08:55:08 +0300

    easier way of using observable

--- main.ts
+++ main.ts
@@ -3,19 +3,8 @@ import { Observable, Observer } from "rxjs";
 let numbers = [1, 5, 10];
 let source = Observable.from(numbers);
 
-class MyObserver implements Observer<number> {
-    next(value) {
-        console.log(`value: ${value}`);
-    }
-
-    error(e) {
-        console.log(`error: ${e}`);
-    }
-
-    complete() {
-        console.log("complete");
-    }
-}
-
-source.subscribe(new MyObserver);
-source.subscribe(new MyObserver);
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    error => console.log(`error: ${error}`),
+    () => console.log("complete")
+);
