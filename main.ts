1e8cd356aea8ee52251b01b484399d5225640e15
lesnikovskiy <lesnikovski@gmail.com>
2017-05-13 22:34:09 +0300

    revisiting retryWhen state on error

--- main.ts
+++ main.ts
@@ -2,41 +2,28 @@ import { Observable } from "rxjs";
 
 import { load, loadWithFetch } from "./loader";
 
-let source = Observable.merge(
-    Observable.of(1),
-    Observable.from([2, 3, 4]),
-    Observable.throw(new Error("Stop!")),
-    Observable.of(5)
-).catch(e => {
-    console.log(`caught: ${e}`);
-    return Observable.of(10);
-});
+const output = document.getElementById("output");
+const button = document.getElementById("button");
 
-source.subscribe(
-    value => console.log(`value: ${value}`),
-    e => console.log(`Error: ${e}`),
-    () => console.log("Complete")
+let click = Observable.fromEvent(button, "click");
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerHTML = m.title;
+        output.appendChild(div);
+    });
+}
+
+loadWithFetch("moviess.json").subscribe(
+    renderMovies,
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")
 );
 
-// const output = document.getElementById("output");
-// const button = document.getElementById("button");
-
-// let click = Observable.fromEvent(button, "click");
-
-// function renderMovies(movies) {
-//     movies.forEach(m => {
-//         let div = document.createElement("div");
-//         div.innerHTML = m.title;
-//         output.appendChild(div);
-//     });
-// }
-
-//loadWithFetch("movies.json").subscribe(renderMovies);
-// //load("movies.json").subscribe(renderMovies);
-
-// click.flatMap(e => loadWithFetch("movies.json"))
-//     .subscribe(
-//     movies => renderMovies(movies),
-//     error => console.log(`error: ${error}`),
-//     () => console.log("complete")
-// );
+click.flatMap(e => loadWithFetch("moviess.json"))
+    .subscribe(
+    movies => renderMovies(movies),
+    error => console.log(`error: ${error}`),
+    () => console.log("complete")
+);
