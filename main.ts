4b796824714a63a3a558738cddc38815af9e112e
lesnikovskiy <lesnikovski@gmail.com>
2017-05-13 22:21:08 +0300

    dealing with error: catching error

--- main.ts
+++ main.ts
@@ -2,12 +2,14 @@ import { Observable } from "rxjs";
 
 import { load, loadWithFetch } from "./loader";
 
-let source = Observable.create(observer => {
-    observer.next(1);
-    observer.next(2);
-    observer.error("Stop!");
-    observer.next(3);
-    observer.complete();
+let source = Observable.merge(
+    Observable.of(1),
+    Observable.from([2, 3, 4]),
+    Observable.throw(new Error("Stop!")),
+    Observable.of(5)
+).catch(e => {
+    console.log(`caught: ${e}`);
+    return Observable.of(10);
 });
 
 source.subscribe(
