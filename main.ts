7c1579ff4820b1575c630239fd3907c2ec85ba3a
lesnikovskiy <lesnikovski@gmail.com>
2017-05-13 22:15:54 +0300

    Dealing with errors: how observable handles errors

--- main.ts
+++ main.ts
@@ -1,59 +1,40 @@
 import { Observable } from "rxjs";
 
-const output = document.getElementById("output");
-const button = document.getElementById("button");
+import { load, loadWithFetch } from "./loader";
 
-let click = Observable.fromEvent(button, "click");
+let source = Observable.create(observer => {
+    observer.next(1);
+    observer.next(2);
+    observer.error("Stop!");
+    observer.next(3);
+    observer.complete();
+});
 
-function load(url: string) {
-    return Observable.create(observer => {
-        const xhr = new XMLHttpRequest();
-
-        xhr.addEventListener("load", () => {
-            if (xhr.status === 200) {
-                let data = JSON.parse(xhr.responseText);
-                observer.next(data);
-                observer.complete();
-            } else {
-                observer.error(xhr.statusText);
-            }
-
-        });
-
-        xhr.open("GET", url);
-        xhr.send();
-    }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
-}
-
-function loadWithFetch(url: string) {
-    return Observable.defer(() => {
-        return Observable.fromPromise(fetch(url).then(r => r.json()));
-    });
-}
-
-function retryStrategy({attempts = 4, delay = 1000}) {
-    return errors => errors.scan((acc, value) => {
-        console.log(acc, value);
-        return acc + 1;
-    }, 0)
-        .takeWhile(acc => acc < attempts)
-        .delay(delay);
-}
-
-function renderMovies(movies) {
-    movies.forEach(m => {
-        let div = document.createElement("div");
-        div.innerHTML = m.title;
-        output.appendChild(div);
-    });
-}
-
-loadWithFetch("movies.json").subscribe(renderMovies);
-//load("movies.json").subscribe(renderMovies);
-
-click.flatMap(e => loadWithFetch("movies.json"))
-    .subscribe(
-    movies => renderMovies(movies),
-    error => console.log(`error: ${error}`),
-    () => console.log("complete")
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    e => console.log(`Error: ${e}`),
+    () => console.log("Complete")
 );
+
+// const output = document.getElementById("output");
+// const button = document.getElementById("button");
+
+// let click = Observable.fromEvent(button, "click");
+
+// function renderMovies(movies) {
+//     movies.forEach(m => {
+//         let div = document.createElement("div");
+//         div.innerHTML = m.title;
+//         output.appendChild(div);
+//     });
+// }
+
+//loadWithFetch("movies.json").subscribe(renderMovies);
+// //load("movies.json").subscribe(renderMovies);
+
+// click.flatMap(e => loadWithFetch("movies.json"))
+//     .subscribe(
+//     movies => renderMovies(movies),
+//     error => console.log(`error: ${error}`),
+//     () => console.log("complete")
+// );
